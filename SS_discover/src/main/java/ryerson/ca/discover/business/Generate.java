1a51a5b41a2ab85e830443ab2948c8230263523c
deep-patel21 <deepstudios21@gmail.com>
2024-03-23 16:03:49 -0400

    updated imports and package delcarations in discover.business

--- SS_discover/src/main/java/ryerson/ca/discover/business/Generate.java
+++ SS_discover/src/main/java/ryerson/ca/discover/business/Generate.java
@@ -4,6 +4,8 @@
  */
 package ryerson.ca.discover.business;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,9 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-
-//need to import
-import ryerson.ca.frontend.helper;
+import ryerson.ca.discover.helper.User;
 /**
  *
  * @author deeps
